913cc2f843759bd5dc6552a904db77e2085fcbaa
Nick Weiland <nickweiland@gmail.com>
2019-12-30 15:41:04 -0800

    :tada: Implements test app
    [master]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_210629) do
+ActiveRecord::Schema.define(version: 2019_12_30_230335) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,10 +33,17 @@ ActiveRecord::Schema.define(version: 2019_12_30_210629) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.integer "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "docs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
--- test/system/uploads_xls_test.rb
+++ test/system/uploads_xls_test.rb
@@ -1,6 +1,17 @@
+require "application_system_test_case"
+
 class UploadsXlsTest < ApplicationSystemTestCase
   test "upload xls" do
     visit root_url
-    click_on "Choose File"
+
+    page.attach_file("doc[attachment]", Rails.root + "test/fixtures/files/test.txt")
+    click_on "Upload File"
+
+    assert_selector "li", text: "test.txt"
+
+    page.attach_file("doc[attachment]", Rails.root + "test/fixtures/files/test.xls")
+    click_on "Upload File"
+
+    assert_selector "li", text: "test.xls"
   end
 end
